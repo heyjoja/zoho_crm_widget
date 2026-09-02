caff9726fa56ba6bd80010b9b0f2330cf5fc4148
José Martínez <jose@edgeuno.com>
2026-09-01 23:09:14 -0500

    build(plugin): configure React and Webpack setup, and I include the zoho widget configuration

---
+++ web-component/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Apps contacts configurated</h1>
+}
+
+export default App
---
+++ web-component/webpack.config.js
@@ -0,0 +1,55 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+export default {
+    entry: './src/main.jsx',
+    mode: 'development',
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: 'defaults' }],
+                            ['@babel/preset-react', { runtime: 'automatic' }],
+                        ],
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+        }),
+    ],
+
+    devServer: {
+        port: 3000,
+        hot: true,
+        historyApiFallback: true,
+    },
+}
